99691813b856c02912297ae4b4dcefbf6b6a1b83
Ajay <ajaykommana123@gmail.com>
2025-07-30 23:34:23 +0530

    Done for the day

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,59 @@
-import Image from "next/image";
+import Image from 'next/image'
+import CodeSnippet from "@/components/code-snippet";
+import { PageActions } from "@/components/page-header";
+import {  PageHeaderDescription,  PageHeaderHeading,} from "@/components/page-header";
+import { PageHeader } from "@/components/page-header";
+import Pager from "@/components/pager";
+import { Button } from "@/components/ui/button";
+import { code } from "@/config/codeContent";
+import { siteConfig } from "@/config/site";
+import Link from "next/link";
+import { ExternalLink, Mail } from "lucide-react";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    return (
+        <>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+        <h1>
+            My data
+        </h1>
+        {/* <PageHeader>
+        <PageHeaderHeading>Ajay Kommana</PageHeaderHeading>
+        <PageHeaderHeading className="mt-2 text-muted-foreground">
+          A coder by day, problem-solver by night!
+        </PageHeaderHeading>
+        <PageHeaderDescription>
+          I am a dedicated Software Engineer specializing in full-stack
+          application development. I enjoy crafting responsive web solutions
+          using modern technologies like Next.js, React, and Tailwind CSS.
+          Currently, I am expanding my skills into mobile development with React
+          Native and Expo, aiming to deliver comprehensive, user-centric
+          software solutions.
+        </PageHeaderDescription>
+        <PageActions>
+          <Button asChild size="sm" className="rounded-md">
+            <Link href={siteConfig.links.resume} target="_blank">
+              Get Resume
+              <ExternalLink className="size-3" strokeWidth={2} />
+            </Link>
+          </Button>
+          <Button asChild size="sm" variant="ghost" className="rounded-md">
+            <Link href={siteConfig.links.email}>
+              <Mail className="size-4" />
+              Send Mail
+            </Link>
+          </Button>
+        </PageActions>
+      </PageHeader> */}
+
+      {/* <CodeSnippet title="welcome.ts" code={code.welcome} /> */}
+
+      {/* <Pager
+        prevHref="/"
+        nextHref="/about"
+        prevTitle="Previous"
+        nextTitle="About Me"
+      /> */}
+        </>
+    )
 }
---
+++ config/site.ts
@@ -0,0 +1,24 @@
+export const siteConfig = {
+  name: "iamajay.tech",
+  url: "https://iamajay.tech",
+  ogImage: "https://abhishekg.com.np/og.jpg",
+  description: "Portfolio of Ajay Kommana",
+  links: {
+    shadcn: "https://ui.shadcn.com/",
+    telegram: "https://t.me/ajay",
+    twitter: "https://x.com/ajay",
+    github: "https://github.com/ajaycodes7",
+    githubProfile: "https://github.com/ajaycodes7",
+    resume: "https://drive.google.com",
+    email: "mailto:ajaykommana123@gmail.com",
+    githubUsername: "ajayCodes7",
+  },
+  bitinfonepal: "https://bitinfonepal.com",
+};
+
+export type SiteConfig = typeof siteConfig;
+
+export const META_THEME_COLORS = {
+  light: "#ffffff",
+  dark: "#09090b",
+};
